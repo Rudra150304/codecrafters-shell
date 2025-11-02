d88ee8c32af252384c4f7a75f7f639d0f0130533
Rudra150304 <rudra.p.s15032004@gmail.com>
2025-11-02 19:33:11 +0530

    Final commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
+
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,11 +9,7 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
-  std::unordered_map<std::string, bool> commands;
-
   std::string command;
   std::getline(std::cin, command);
-  if(!commands.count(command)){
-    std::cout << command << ": command not found" << std::endl;;
-  }
+  std::cout << command << ": command not found" << std::endl;
 }
