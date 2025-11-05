26a84c12ceb509a81157320f598b2d79e2b9e8af
Rudra150304 <rudra.p.s15032004@gmail.com>
2025-11-05 09:00:46 +0530

    PATH

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cstdlib>
+#include <filesystem>
+#include <unistd.h>
+
+namespace fs = std::filesystem;
+
+std::string find_in_path(const std::string& cmd){
+  char* path_env = std::getenv("PATH");
+  if(!path_env)
+    return "";
+
+  std::string path_str(path_env);
+  std::istringstream path_stream(path_str);
+  std::string dir;
+
+  while(std::getline(path_stream, dir, ':')){
+    fs::path file_path = fs::path(dir) / cmd;
+    if(fs::exists(file_path) && fs::is_regular_file(file_path) && (access(file_path.c_str(), X_OK) == 0)){
+      return file_path.string();
+    }
+  }
+  return "";
+}
 
 
 int main() {
@@ -23,6 +46,7 @@ int main() {
     std::istringstream iss(command);
     std::string cmd;
     iss >> cmd;
+
     std::string rest;
     std::getline(iss, rest);
     if(!rest.empty() && rest[0] == ' ')
@@ -42,7 +66,13 @@ int main() {
         std::cout << "type is a shell builtin" << std::endl;
       }
       else{
-        std::cout << rest << ": not found" << std::endl;
+        std::string pt = find_in_path(rest);
+        if(pt.empty()){
+          std::cout << rest << ": not found" << std::endl;
+        }
+        else{
+          std::cout << rest << " is " << pt << std::endl;
+        }
       }
     }
     else{
